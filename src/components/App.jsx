0bb522d508238580257b5ef736ad0d3974878c8f
Student <example@example.com>
2020-02-17 14:04:29 -0800

    add code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,28 +4,54 @@ import Header from "./Header";
 import TicketList from "./TicketList"
 import Footer from "./Footer"
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
 
 
-function App(){
-  var allStyle = {
-    backgroundColor: '#13B7FF',
-    fontFamily: "'Nosifer', cursive",
-    color: '#BD0000',
-    textAlign: 'left',
-    margin: '-8px',
-    textShadow: '5px 3px 5px black'
- }
-  return (
-    <div style={allStyle}>
-      <Header/>
-      <Switch>
-     <Route exact path='/' component={TicketList} />
-     <Route path='/newticket' component={NewTicketForm} />
-   </Switch>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+  var newMasterTicketList = this.state.masterTicketList.slice();
+  newMasterTicketList.push(newTicket);
+  this.setState({masterTicketList: newMasterTicketList});
+}
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
+        </Switch>
+        <Footer/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
+
+
+
+
+
+
+    //   var allStyle = {
+      // backgroundColor: '#13B7FF',
+      // fontFamily: "'Nosifer', cursive",
+      // color: '#BD0000',
+      // textAlign: 'left',
+      // margin: '-8px',
+      // textShadow: '5px 3px 5px black'
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,40 +1,25 @@
 import React from "react";
-import Ticket from "./Ticket"
-
-var masterTicketList = [
-  {
-    names: "Casey & Tyler" ,
-    location: "7B",
-    issue: "all your bases are belong to me"
-  },
-  {
-    names: 'Thanos & Gamorrah',
-    location: '3A',
-    issue: 'The Avengers'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Donkey picture not displaying'
-  },
-]
+import Ticket from "./Ticket";
+import PropTypes from 'prop-types';
 
 
-function TicketList(){
-  var listStyle = {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr 1fr',
-  }
+
+function TicketList(props){
+    console.log(props.ticketList);
   return (
-      <div style={listStyle}>
-
-        {masterTicketList.map((ticket, index) =>
-          <Ticket names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue} />
-        )}
-      </div>
-    );
-  }
+    <div>
+      <hr/>
+      {props.ticketList.map((ticket) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={ticket.id}/>
+      )}
+    </div>
+  );
+}
+  TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
   export default TicketList;
